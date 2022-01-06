0a5e2b0aec81bfae3adbb37c4c964ac2a7411d9e
ivanburda1986 <burda.ivan@gmail.com>
2022-01-06 01:38:06 +0100

    writing tests

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -1,11 +1,35 @@
-import { Shop, Item } from '../gilded_rose';
+import { Shop, Item } from "../gilded_rose";
 
 describe("Gilded Rose", () => {
+  it("should add a new item", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    const addedItem = gildedRose.items[0];
+    expect(addedItem.name).toEqual("foo");
+    expect(addedItem.sellIn).toEqual(1);
+    expect(addedItem.quality).toEqual(1);
+  });
 
-    it("should foo", () => {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
-    });
+  it("should degrade the quality and decrease the sellIn of a regular item", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    const addedItem = gildedRose.items[0];
+    gildedRose.updateQuality();
+    expect(addedItem.sellIn).toEqual(0);
+    expect(addedItem.quality).toEqual(0);
+  });
 
+  it("should never increase quality of an item above 50", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 10, 50), new Item("foo", 1, 1), new Item("foo", 1, 1)]);
+    const addedItem = gildedRose.items[0];
+    gildedRose.updateQuality();
+    expect(addedItem.sellIn).toEqual(0);
+    expect(addedItem.quality).toEqual(0);
+  });
+
+  it("should twice as fast degrade the quality and decrease the sellIn of a regular item for which the sell by date has elapsed", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 4)]);
+    const addedItem = gildedRose.items[0];
+    gildedRose.updateQuality();
+    expect(addedItem.sellIn).toEqual(-1);
+    expect(addedItem.quality).toEqual(2);
+  });
 });
--- src/gilded_rose.ts
+++ src/gilded_rose.ts
@@ -17,29 +17,9 @@ export class Shop {
     this.items = items;
   }
 
-  updateQualityRefactored() {
+  updateQuality() {
     const decreaseQuality = ({ item, decrement = 1 }: { item: Item; decrement?: number }): number => {
-      // if (item.name === "Quadro" && item.sellIn < 0 && item.quality >= 8) {
-      //   return (item.quality -= 8);
-      // }
-      // if (item.name === "Quadro" && item.quality >=4) {
-      //   return (item.quality -= 4);
-      // }
-
-      // if (item.name === "Tripled" && item.sellIn < 0 && item.quality >= 6) {
-      //   return (item.quality -= 6);
-      // }
-      // if (item.name === "Tripled" && item.quality >= 3) {
-      //   return (item.quality -= 3);
-      // }
-
-      // if (item.name === "Conjured" && item.sellIn < 0 && item.quality >= 4) {
-      //   return (item.quality -= 4);
-      // }
-      // if (item.name === "Conjured" && item.quality >= 2) {
-      //   return (item.quality -= 2);
-      // }
-      if (item.sellIn < 0 && item.quality >= 2 * decrement) {
+      if (item.sellIn <= 0 && item.quality >= 2 * decrement) {
         return (item.quality -= 2 * decrement);
       }
       if (item.quality >= decrement) {
@@ -48,10 +28,7 @@ export class Shop {
       return 0;
     };
     const decreaseSellIn = (item: Item): number => {
-      if (item.sellIn > 0) {
-        return (item.sellIn -= 1);
-      }
-      return 0;
+      return (item.sellIn -= 1);
     };
     const increaseQuality = ({ item, increment }: { item: Item; increment: number }): number => {
       if (item.quality < 50) {
@@ -89,73 +66,6 @@ export class Shop {
           return;
       }
     });
-  }
-
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      //For all items except of: Aged Brie, Backstage
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          //If their quality is above 0
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            // And they are not Sulfuras
-            this.items[i].quality = this.items[i].quality - 1; //Decrease the quality by -1
-          }
-        }
-      } else {
-        //For Aged Brie and Backstage if their quality is below 50
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1; //Increase the quality by 1
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            //And for Backgstage:
-            if (this.items[i].sellIn < 11) {
-              //If its sellIn is <11
-              if (this.items[i].quality < 50) {
-                //and its quality <50, increase its quality +1
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              //And if its sellIn is <6
-              if (this.items[i].quality < 50) {
-                //and its quality <50, increase its quality +1
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      //For all items except of: Sulfuras
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1; //Decrease their sellIn by -1
-      }
-      if (this.items[i].sellIn < 0) {
-        //If the item's sellIn is <0, i.e. it has elapse
-        if (this.items[i].name != "Aged Brie") {
-          //And if the item is not Aged Brie
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            //And the item is not Backstage
-            if (this.items[i].quality > 0) {
-              //And its quality is >0
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                //And the items is not Sulfuras
-                this.items[i].quality = this.items[i].quality - 1; // Decrease the items quality by -1 -> the quality is decreasing twice as fast
-              }
-            }
-          } else {
-            // But the item is Backstage
-            this.items[i].quality = this.items[i].quality - this.items[i].quality; //Set the items quality to 0
-          }
-        } else {
-          //But if the item is not "Aged Brie"
-          if (this.items[i].quality < 50) {
-            //And its qualit is <50
-            this.items[i].quality = this.items[i].quality + 1; //Increase its quality
-          }
-        }
-      }
-    }
-
     return this.items;
   }
 }
