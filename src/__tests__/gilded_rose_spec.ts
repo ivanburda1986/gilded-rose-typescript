fb923eb2bf10f8e31fa41c9064003502151bea21
ivanburda1986 <burda.ivan@gmail.com>
2022-01-07 12:33:04 +0100

    tests

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -9,20 +9,25 @@ describe("Gilded Rose", () => {
     expect(addedItem.quality).toEqual(1);
   });
 
-  it("should degrade the quality and decrease the sellIn of a regular item", () => {
+  it("should degrade the quality of a regular item", () => {
     const gildedRose = new Shop([new Item("foo", 1, 1)]);
     const addedItem = gildedRose.items[0];
     gildedRose.updateQuality();
-    expect(addedItem.sellIn).toEqual(0);
     expect(addedItem.quality).toEqual(0);
   });
 
   it("should never increase quality of an item above 50", () => {
-    const gildedRose = new Shop([new Item("Aged Brie", 10, 50), new Item("foo", 1, 1), new Item("foo", 1, 1)]);
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 50)]);
     const addedItem = gildedRose.items[0];
     gildedRose.updateQuality();
-    expect(addedItem.sellIn).toEqual(0);
-    expect(addedItem.quality).toEqual(0);
+    expect(addedItem.quality).toEqual(50);
+  });
+
+  it("should never change the quality of Sulfuras, Hand of Ragnaros which is constantly 80", () => {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 80)]);
+    const addedItem = gildedRose.items[0];
+    gildedRose.updateQuality();
+    expect(addedItem.quality).toEqual(80);
   });
 
   it("should twice as fast degrade the quality and decrease the sellIn of a regular item for which the sell by date has elapsed", () => {
