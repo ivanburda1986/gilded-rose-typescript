6a9e4ce707a6b5a561fab3d254e3c96171841b2e
iBurda <burda.ivan@gmail.com>
2022-03-16 23:59:21 +0100

    tests

--- round03/src/__tests__/gilded_rose_spec.ts
+++ round03/src/__tests__/gilded_rose_spec.ts
@@ -2,7 +2,7 @@ import { Shop, Item } from '../gilded_rose';
 
 describe("Gilded Rose", () => {
 
-    it("should foo", () => {
+    it("An item has sellIn and quality value", () => {
         const gildedRose = new Shop([new Item("foo", 0, 0)]);
         const items = gildedRose.updateQuality();
         expect(items[0].name).toEqual("fixme");
