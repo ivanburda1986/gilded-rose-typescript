bfa7d7d593ccc180df7ee4a310c17ed075725678
iBurda <burda.ivan@gmail.com>
2022-03-21 23:58:21 +0100

    gilded rose

--- round03/src/normalItem/__tests__/normalItem.test.ts
+++ round03/src/normalItem/__tests__/normalItem.test.ts
@@ -1,10 +1,18 @@
 import {Item, Shop} from "../../gilded_rose";
+const NORMAL_ITEM = "Normal Item";
+const SELL_IN_POSITIVE = 5;
+const QUALITY_POSITIVE = 5;
+
+function getShopWithItem(name: string, sellIn: number, quality: number){
+    return new Shop([new Item(name, sellIn, quality)]);
+}
 
 describe("Normal item", () => {
     it("has sellIn and quality value", () => {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+        const gildedRose = getShopWithItem(NORMAL_ITEM,SELL_IN_POSITIVE, QUALITY_POSITIVE);
+        const items = gildedRose.items[0];
+        expect(items[0].sellIn).toEqual(SELL_IN_POSITIVE);
+        expect(items[0].quality).toEqual(QUALITY_POSITIVE);
     });
 
     it("A normal item decreases sellIn by 1 every day", () => {
